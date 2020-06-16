b0b785b0ec8dda4ec0ba28773d8668ffabcfd6a2
5x <5x@users.noreply.github.com>
2020-06-17 00:58:59 +0300

    Updated gh-pages

--- .stylelintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  "extends": "stylelint-config-standard",
-  "plugins": ["stylelint-scss"],
-  "rules": {
-    "at-rule-no-unknown": null,
-    "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
-  },
-}
--- src/script/utils.js
+++
@@ -1,5 +0,0 @@
-export const MS_IN_MINUTE = 60000;
-
-export function escapeStringRegExp(str) {
-  return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
-}
